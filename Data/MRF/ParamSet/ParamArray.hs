2fe68a3c6754a56a379f86309126253b3c9876e1
Jakub Waszczuk <waszczuk.kuba@gmail.com>
2012-02-15 12:13:15 +0100

    Get rid of undefined values in ParamArray

--- Data/MRF/ParamSet/ParamArray.hs
+++ Data/MRF/ParamSet/ParamArray.hs
@@ -7,7 +7,7 @@ import qualified Data.Array.Unboxed as A
 import qualified Data.Vector.Unboxed as U
 import           Data.Binary (Binary, put, get)
 import           Control.Monad (forM_)
-import           Data.Ix (Ix, inRange)
+import qualified Data.Ix as Ix -- (Ix, range, inRange)
 
 import           Data.MRF.Vector.Binary
 import           Data.MRF.Generic.ParamSet
@@ -20,7 +20,7 @@ data ParamArray f = ParamArray
     , values :: U.Vector Double }
 
 
-instance (Ix f, Binary f) => Binary (ParamArray f) where
+instance (Ix.Ix f, Binary f) => Binary (ParamArray f) where
     put params = do
         put $ ixs params
         put $ values params
@@ -41,14 +41,15 @@ instance ParamCore (ParamArray f) where
         return $ ParamArray (ixs params) values' 
 
 
-instance (Ix f, Feature f c x) => ParamSet (ParamArray f) f c x where
+instance (Ix.Ix f, Feature f c x) => ParamSet (ParamArray f) f c x where
 
     fromList paramList =
         ParamArray ixs values
       where
         feats = map fst paramList
         values = U.fromList $ map snd paramList
-        ixs = A.array bounds $ zip feats [0..]
+        ixs = A.array bounds [(key, -1) | key <- Ix.range bounds]
+         A.// zip feats [0..]
         bounds = (minimum feats, maximum feats)
 
     size = U.length . values
@@ -56,10 +57,14 @@ instance (Ix f, Feature f c x) => ParamSet (ParamArray f) f c x where
     featureToIx feat ps =
         ixs ps A.! feat
 
-    phi ps feat =
-        if inRange (A.bounds $ ixs ps) feat
-            then (values ps U.!) (ixs ps A.! feat)
-            else 0.0
+    phi ps feat = doPhi
+      where
+        doPhi
+            | not inRange   = 0.0
+            | ix < 0        = 0.0
+            | otherwise     = values ps U.! ix
+        inRange = Ix.inRange (A.bounds $ ixs ps) feat
+        ix      = ixs ps A.! feat
 
-mkParamArray :: (Ix f, Feature f c x) => [f] -> ParamArray f
+mkParamArray :: (Ix.Ix f, Feature f c x) => [f] -> ParamArray f
 mkParamArray = mkParamSet
